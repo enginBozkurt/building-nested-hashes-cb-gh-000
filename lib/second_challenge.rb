43139795ca8244c6e3a97020720b622a2745cd15
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-28 13:38:35 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,10 +6,16 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
+      :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
       }}
 
-  
+
 end
