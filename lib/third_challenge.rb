9ae9aa502f913bd0307d30bff47210b6b9a1c1cf
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-28 13:45:30 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :patriarch => {name: "Lord Montague", age: "53"},
         :matriarch => {name: "Lady Montague", age: "54"},
         :hero => {name: "Romeo", age: "15", status: "alive"},
-        :hero_friends => []    
+        :hero_friends => []
    },
    :capulet => {
      :patriarch => {name: "Lord Capulet", age: "50"},
